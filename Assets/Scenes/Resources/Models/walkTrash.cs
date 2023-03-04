eade863255dfd2016ca3d7fbba0c2097dc5a5ff3
brontozavrist <125477023+brontozavrist@users.noreply.github.com>
2023-03-04 17:37:45 +0500

    LvlUpd

---
+++ Assets/Scenes/Resources/Models/walkTrash.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float verticalMovement = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(horizontalMovement, 0, verticalMovement);
+    }
+}
